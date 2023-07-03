42715022913d4b9de59752043864699dc7e4f986
TheNiska <denis.bond.x@gmail.com>
2023-07-03 16:35:51 +0300

    added lists

---
+++ lists.py
@@ -0,0 +1,32 @@
+nums = [4, 2, 9, 10, 15, 40, 1, -3]
+empty_list = list()
+
+# list can hold different data types
+different_types = ["one", 2, True]
+
+# Lists methods --------------------------------------------------------------
+length = len(nums)  # length of the list
+nums.append(10)  	# appendiing to the end of the list
+
+position = 1
+element = 2
+nums.insert(position, element)  # inserting element 2 in position 1
+last = nums.pop()  # returns and removes the last item
+nums.remove(15)  # removes the first matching element
+different_types.clear()  # removes all elements from the list
+
+nums.reverse()  # reverse the list
+nums.sort()     # sorts the list in place
+nums.sort(reverse=True)  # sort descending
+new_nums = sorted(nums)  # creates new  sorted list
+
+one_to_four = [1, 2, 3, 4]
+cp = one_to_four.copy()  # copy
+# ----------------------------------------------------------------------------
+
+# lists comprehension
+a = [1, 2, 3, 4, 5]
+squares = [i * i for i in a]
+
+print(squares)
+print(nums)
